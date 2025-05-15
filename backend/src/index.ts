cb3d1b15d6f1b4d4fc22390cff0ba698bc1e37f6
DESKTOP-0089\Hui Qing <huiqing.lau@pixent.net>
2025-05-15 18:52:36 +0800

    Init typescript dependency, express...

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+    res.send("Hello from PKM backend!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
