0c0b4b0f5617677fbea4ce114df8f584e325a48e
Hui Qing <qing05099@gmail.com>
2025-06-18 12:03:37 +0800

    test gitflow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,8 @@ app.get("/", (_req, res) => {
     res.send("Hello from PKM backend!");
 });
 
+console.log("hello~~");
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
